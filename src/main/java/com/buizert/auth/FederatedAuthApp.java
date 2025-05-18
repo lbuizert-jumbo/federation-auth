176c10f8bc8f8808b03720ddac8894bb12e02d71
Leen Buizert <leen@buizert-consultancy.nl>
2025-05-18 21:02:56 +0200

    Initial commit

---
+++ src/main/java/com/buizert/auth/FederatedAuthApp.java
@@ -0,0 +1,46 @@
+package com.buizert.auth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.buizert.auth.helpers.TokenLogger;
+
+/**
+ * Generate application:
+ * mvn clean verify
+ * 
+ * Run application:
+ * 
+ * @author Leen Buizert
+ *
+ */
+
+@SpringBootApplication
+public class FederatedAuthApp implements CommandLineRunner {
+	
+	@Autowired
+	private LocalTokenManager tokenManager;
+	
+	@Autowired
+	private FederatedTokenManager federatedTokenManager;
+	
+	private static Logger log = LoggerFactory.getLogger(FederatedAuthApp.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FederatedAuthApp.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		final String localToken = tokenManager.getFederationToken();
+		TokenLogger.log(localToken);
+		log.info("Use this token to request token from external IDP");
+		final String federatedToken = federatedTokenManager.getBearerToken(localToken);
+		TokenLogger.log(federatedToken);
+	}
+}
